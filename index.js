d84f86b8178338c6098898183bede2c93c4265db
Guilherme (Coder) de Souza <gui_souza@me.com>
2014-11-23 19:39:58 -0200

    linking submodules

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-/*       .#.                                                       
-        @@@@@                                                      
-        @@@@@                                                      
-          @                                                        
-    .....@@@                                                       
-   .@@@@@@@                                                        
-   @@@@@@@                                                         
-    @@@@@@@:@@@..@@@@@@@  @@@   @@@ @@@@@@@@   @@@@@@@      @@@@   
-   .@@@@@@@@    @@@@@@@@  @@@   @@@ @@@@@@@@@  @@@@@@@@@  @@@@@@@@ 
+/*       .#.
+        @@@@@
+        @@@@@
+          @
+    .....@@@
+   .@@@@@@@
+   @@@@@@@
+    @@@@@@@:@@@..@@@@@@@  @@@   @@@ @@@@@@@@   @@@@@@@      @@@@
+   .@@@@@@@@    @@@@@@@@  @@@   @@@ @@@@@@@@@  @@@@@@@@@  @@@@@@@@
    '@@@@@@@@@@@@@@@       @@@   @@@ @@@    @@@ @@@   @@@ @@@    @@@
 :@@@@@@@@@:     @@@@@@@@  @@@   @@@ @@@@@@@@@  @@@@@@@@@ @@@    @@@
   `@@@@:             @@@  @@@   @@@ @@@#@@@    @@@@@@@@  @@@@@@@@@@
@@ -22,17 +22,17 @@
 */
 
 module.exports = Supra.Class.extend({
-	/**
-	 * [init description]
-	 * @return {[type]} [description]
-	 */
-	init:function(){
+  /**
+   * [init description]
+   * @return {[type]} [description]
+   */
+  init:function(){
     console.log(Supra.prefs.port);
-		App = Supra.http.createServer(Supra.request)
-		.listen(Supra.prefs.port,function(){
-			console.log(Supra.prefs.server+' Up and running on port '+Supra.prefs.port+'\n'+
-				'Report any suggestion to gui_souza@me.com\n'+
-				'I hope you like it.')
-		});
-	}
+    App = Supra.http.createServer(Supra.request)
+    .listen(Supra.prefs.port,function(){
+      console.log(Supra.prefs.server+' Up and running on port '+Supra.prefs.port+'\n'+
+        'Report any suggestion to gui_souza@me.com\n'+
+        'I hope you like it.')
+    });
+  }
 })
